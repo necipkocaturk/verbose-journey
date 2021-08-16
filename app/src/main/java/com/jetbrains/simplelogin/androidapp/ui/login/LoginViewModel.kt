70f804246c49f1a7a62070f93da6c9a3185493ed
necip <necipkocaturk@hotmail.com>
2021-08-16 22:56:42 +0300

    ilk commit mesajı

---
+++ app/src/main/java/com/jetbrains/simplelogin/androidapp/ui/login/LoginViewModel.kt
@@ -0,0 +1,39 @@
+package com.jetbrains.simplelogin.androidapp.ui.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import android.util.Patterns
+import com.jetbrains.simplelogin.androidapp.data.LoginRepository
+import com.jetbrains.simplelogin.androidapp.data.Result
+
+import com.jetbrains.simplelogin.androidapp.R
+import com.jetbrains.simplelogin.androidapp.data.LoginDataValidator
+import com.jetbrains.simplelogin.androidapp.data.LoginDataValidator.Result.*
+
+class LoginViewModel(private val loginRepository: LoginRepository, private val dataValidator: LoginDataValidator) : ViewModel() {
+
+    private val _loginForm = MutableLiveData<LoginFormState>()
+    val loginFormState: LiveData<LoginFormState> = _loginForm
+
+    private val _loginResult = MutableLiveData<LoginResult>()
+    val loginResult: LiveData<LoginResult> = _loginResult
+
+    fun login(username: String, password: String) {
+        // can be launched in a separate asynchronous job
+        val result = loginRepository.login(username, password)
+
+        if (result is Result.Success) {
+            _loginResult.value = LoginResult(success = LoggedInUserView(displayName = result.data.displayName))
+        } else {
+            _loginResult.value = LoginResult(error = R.string.login_failed)
+        }
+    }
+
+    fun loginDataChanged(username: String, password: String) {
+        _loginForm.value = LoginFormState(
+            usernameError = (dataValidator.checkUsername(username) as? Error)?.message,
+            passwordError = (dataValidator.checkPassword(password) as? Error)?.message
+        )
+    }
+}
